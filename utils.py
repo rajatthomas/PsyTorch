b19c80833fa0fc4d9526c62bf14c95c98c338e4e
rajat <rajatthomas@gmail.com>
2018-02-28 13:45:24 +0100

    added better comments

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+
 def create_mri_patch(img, patch_dim=32):
-
-
+    '''
+    Function to return a patch extracted from an MRI image
+    :param img: numpy nd-array
+    :param patch_dim: dimension of patch to extract
+    :return: patch of size patch_dim^3
+    '''
     img_dim = np.array(img.shape)
     if np.any( img_dim < patch_dim):
         raise ValueError("Dimension of patch should be less than image")
@@ -11,6 +16,6 @@ def create_mri_patch(img, patch_dim=32):
     corner_low  = np.array([0,0,0])
     corner_high = img_dim - patch_dim
 
-    cx, cy, cz = [ np.random.random_integers(low=corner_low[i], high=corner_high[i] for i in [0,1,2])]
+    cx, cy, cz = [ np.random.random_integers(low=corner_low[i], high=corner_high[i]) for i in [0,1,2]]
 
     return img[cx:patch_dim, cy:patch_dim, cz:patch_dim]
