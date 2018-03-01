0ee35f69f9402613c0360a7e836681845b7d09cf
rajat <rajatthomas@gmail.com>
2018-03-01 17:38:43 +0100

    catch warning img_dim lt patch_dim

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import numpy as np
 import os
 import os.path as pth
+import warnings
 
 import nibabel as nib
 
 
-def create_mri_patch(img_file, patch_dim=64, num_patches=100):
+def create_mri_patch(img_file, patch_dim=64, num_patches=20):
     """
     Function to return a patch extracted from an MRI image
     :param img_file: nii file containing image
@@ -19,7 +20,8 @@ def create_mri_patch(img_file, patch_dim=64, num_patches=100):
     if np.any(img_dim < patch_dim):
         print('image shape -> {0}, patch_dim -> {1}'.format(img_dim, patch_dim))
         print('image filename {}'.format(img_file))
-        raise ValueError("Dimension of patch should be less than image")
+        warnings.warn("Dimension of patch should be less than image; IGNORING file")
+        return None
 
     # Choose a random top corner
     corner_low = np.array([0, 0, 0])
@@ -42,7 +44,7 @@ def read_mri(img_file):
     return nib.load(img_file).get_data()
 
 
-def create_patch_files(root_dir, anat_sub_dir='anat', anat_file='anat.nii.gz', patch_dim=64, num_patches=100):
+def create_patch_files(root_dir, anat_sub_dir='anat', anat_file='anat.nii.gz', patch_dim=64, num_patches=20):
     """
     Create a directory within root_dir containing ASD and CON subdirectories.
     These subdirectories will contain image patches of size patch_dim
@@ -59,22 +61,27 @@ def create_patch_files(root_dir, anat_sub_dir='anat', anat_file='anat.nii.gz', p
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-    subj_dirs = sorted(os.listdir(pth.join(root_dir, 'ABIDE_orig')))
+    dir_orig = pth.join(root_dir, 'ABIDE_orig')
+    subj_dirs = sorted(os.listdir(dir_orig))
 
     tot_patches = 0
     num_dirs = len(subj_dirs)
     for idir, d in enumerate(subj_dirs):
-        img_file = pth.join(root_dir, d, anat_sub_dir, anat_file)
-        all_patches = create_mri_patch(img_file, patch_dim=patch_dim, num_patches=num_patches)
+        img_file = pth.join(dir_orig, d, anat_sub_dir, anat_file)
+        tmp_ = create_mri_patch(img_file, patch_dim=patch_dim, num_patches=num_patches)
 
-        if not os.path.exists(pth.join(dir_name, d)):
-            os.makedirs(pth.join(dir_name, d))
+        # Continue if no patches are returned or directory already exists
+        if tmp_ is None or os.path.exists(pth.join(dir_name, d)):
+            continue
+
+        all_patches = tmp_
+        os.makedirs(pth.join(dir_name, d))
 
         for patch_i in range(num_patches):
             patch_file = pth.join(dir_name, d, 'patch_'+str(patch_i)+'.npy')
             np.save(patch_file, all_patches[patch_i])
 
-        if idir % 10 == 0:
+        if (idir+1) % 10 == 0:
             print('Completed {0:0.2f}% of {1} directories'.format(idir/num_dirs*100, num_dirs))
 
         tot_patches += num_patches
